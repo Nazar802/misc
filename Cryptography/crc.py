b1cc0d5b5e79671596e35dcf48dc1ab423575461
Nazar802 <67698658+Nazar802@users.noreply.github.com>
2021-11-07 20:50:21 +0200

    Delete Cryptography directory

--- Cryptography/crc.py
+++
@@ -1,8 +0,0 @@
-import zlib
-
-def check(file):
-    return "%X"%(zlib.crc32(open(file, 'rb').read()) & 0xFFFFFFFF)
-
-
-file = "./dung1.txt"
-print(check(file))        
--- Cryptography/modexp.py
+++
@@ -1,8 +0,0 @@
-num=2813
-mod=3223
-i=0
-
-for i in range(11):
-    num=pow(num,2,mod)
-    i+=1
-    print(2**i, num)
